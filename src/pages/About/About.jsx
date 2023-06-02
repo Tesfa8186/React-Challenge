2d4ea810305367e8d8232bf09fb4883259037924
Tesfa8186 <tesfa1978@gmail.com>
2023-06-03 03:55:48 +0800

    Updating Portfolio files

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -9,7 +9,7 @@ import css from './About.module.css';
           <img width="350px" alt="Profilephoto" src={`${process.env.PUBLIC_URL}/assets/images/Profilephoto.jpg`}></img>
           <p className={`col s12 ${css.aboutText}`}>
           Hello My name is Tesfa Weldemichael and lives in Perth Australia since 2004. I have family with 2 kids.
-          <p>I am Full Stack Junior Website Developer from University of Western Australia (UWA).</p>
+          <p>I am Full Stack Website Developer Graduated from University of Western Australia (UWA).</p>
           <p>I love to expand my skills with continuous practice & dedicated myself.</p> 
           </p>
         </div>
